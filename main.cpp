63f873d94b407039d1b26cbecd6050b3956b76aa
ZHU Bob Hong-yu <aglax2357@icloud.com>
2017-04-29 18:25:47 +0800

    KNN

--- main.cpp
+++ main.cpp
@@ -5,41 +5,24 @@
 //
 
 #include <stdio.h>
+#include <fstream>
 #include "RTree.h"
 
-struct Rect
+#define NUMDIMS 3
+#define FEATUREDIMS 9
+
+class Point
 {
-    Rect()  {}
-    
-    Rect(float a_minX, float a_minY, float a_maxX, float a_maxY)
-    {
-        min[0] = a_minX;
-        min[1] = a_minY;
-        
-        max[0] = a_maxX;
-        max[1] = a_maxY;
-    }
-    
-    
-    float min[2];
-    float max[2];
+public:
+    float data[FEATUREDIMS];
 };
 
-struct Rect rects[] =
+void ReadFeatures(std::ifstream &a_input, int totalNums, Point* a_pointArray)
 {
-    Rect(2, 2, 2, 2), // xmin, ymin, xmax, ymax (for 2 dimensional RTree)
-    Rect(5, 7, 5, 7),
-    Rect(8, 5, 8, 5),
-    Rect(9, 2, 9, 2),
-    Rect(8, 12, 8, 12),
-    Rect(8, 12.4, 8, 12.4),
-    Rect(1, 2, 1, 2),
-    Rect(1, 1, 1, 1),
-};
-
-int nrects = sizeof(rects) / sizeof(rects[0]);
-
-Rect search_rect(8.2, 10, 8.35, 12); // search will find above rects that this one overlaps
+    for(int i = 0; i < totalNums; i++)
+        for(int j = 0; j < FEATUREDIMS; j++)
+            a_input >> a_pointArray[i].data[j];
+}
 
 
 bool MySearchCallback(int id, void* arg)
@@ -57,34 +40,50 @@ bool MyKNNSearchCallback(int id, void* arg)
 
 int main()
 {
-    RTree<int, float, 2, float> tree;
+    RTree<int, float, 3, float> tree;
     
-    int i, nhits;
-    printf("nrects = %d\n", nrects);
+    int totalNums;
     
-    for(i=0; i<nrects; i++)
+    std::ifstream m_input("/Users/AGLAX/Desktop/R-T/R-T/features.txt");
+    if(!m_input.is_open())
+        printf("no input!\n");
+        
+    m_input >> totalNums;
+    printf("totalNums = %d\n", totalNums);
+    
+    Point* m_pointArray = new Point[totalNums];
+    ReadFeatures(m_input, totalNums, m_pointArray);
+    
+    for(int i = 0; i <totalNums; i++)
     {
-        tree.Insert(rects[i].min, rects[i].max, i); // Note, all values including zero are fine in this version
+        tree.Insert(m_pointArray[i].data, m_pointArray[i].data, i); // Note, all values including zero are fine in this version
+        printf("%d\n", i);
     }
     
-    nhits = tree.Search(search_rect.min, search_rect.max, MySearchCallback, NULL);
-    
+    float searchRange[2][3] = {{1000, 1000, 1000}, {9000, 9000, 9000}};
+    int nhits = tree.Search(searchRange[0], searchRange[1], MySearchCallback, NULL);
     printf("Search resulted in %d hits\n", nhits);
+
+    float kNNSearchPoint[3] = {5000, 5000, 5000};
+    tree.KNNSearch(kNNSearchPoint, MyKNNSearchCallback, 7);
     
+    delete [] m_pointArray;
+    
+    /*
     // Iterator test
     int itIndex = 0;
-    RTree<int, float, 2, float>::Iterator it;
+    RTree<int, float, 3, float>::Iterator it;
     for( tree.GetFirst(it);
         !tree.IsNull(it);
         tree.GetNext(it) )
     {
         int value = tree.GetAt(it);
         
-        float boundsMin[2] = {0,0};
-        float boundsMax[2] = {0,0};
+        float boundsMin[3] = {0,0,0};
+        float boundsMax[3] = {0,0, 0};
         it.GetBounds(boundsMin, boundsMax);
-        printf("it[%d] %d = (%f,%f,%f,%f,%f)\n", itIndex++, value, boundsMin[0], boundsMin[1], boundsMax[0], boundsMax[1],
-               (boundsMin[0] - 9) * (boundsMin[0] - 9) + (boundsMin[1] - 9) * (boundsMin[1] - 9));
+        printf("it[%d] %d = (%f,%f,%f,%f,%f, %f, %f)\n", itIndex++, value, boundsMin[0], boundsMin[1],  boundsMin[2], boundsMax[0], boundsMax[1], boundsMax[2],
+               (boundsMin[0] - 9) * (boundsMin[0] - 9) + (boundsMin[1] - 9) * (boundsMin[1] - 9) + (boundsMin[2] - 9) * (boundsMin[2] - 9));
     }
     
     // Iterator test, alternate syntax
@@ -97,23 +96,9 @@ int main()
         printf("it[%d] %d\n", itIndex++, value);
     }
     
-    Rect KNNsearch_rect(9, 9, 0, 0);
-    tree.KNNSearch(KNNsearch_rect.min, MyKNNSearchCallback);
+    Rect KNNsearch_rect(9, 9, 9, 0, 0, 0);
+    tree.KNNSearch(KNNsearch_rect.min, MyKNNSearchCallback, 3);*/
     
     getchar(); // Wait for keypress on exit so we can read console output
     
-    // Output:
-    //
-    // nrects = 4
-    // Hit data rect 1
-    // Hit data rect 2
-    // Search resulted in 2 hits
-    // it[0] 0 = (0,0,2,2)
-    // it[1] 1 = (5,5,7,7)
-    // it[2] 2 = (8,5,9,6)
-    // it[3] 3 = (7,1,9,2)  
-    // it[0] 0
-    // it[1] 1
-    // it[2] 2
-    // it[3] 3
 }
